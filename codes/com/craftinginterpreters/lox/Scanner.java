fadc17deed9832ae35bd34552293670d2f2950c6
mina0923 <cimi22051@g.nihon-u.ac.jp>
2024-12-17 13:30:53 +0900

    add GenerateAst.java, generate Expr.java

--- codes/com/craftinginterpreters/lox/Scanner.java
+++ codes/com/craftinginterpreters/lox/Scanner.java
@@ -175,9 +175,17 @@ private void number() {
   }
 
   private void string() {
-    while (peek() != '"' && !isAtEnd()) {
-      if (peek() == '\n') line++;
-      advance();
+    while (!isAtEnd()) {
+      if (peek() == '\n') line++; 
+  
+      if (peek() == '\\' && peekNext() == '"') {
+        advance(); 
+        advance(); 
+      } else if (peek() == '"') {
+        break;
+      } else {
+        advance();
+      }
     }
 
     if (isAtEnd()) {
@@ -190,7 +198,7 @@ private void number() {
 
     // Trim the surrounding quotes.
     String value = source.substring(start + 1, current - 1);
-    addToken(STRING, value);
+    addToken(STRING, value.replace("\\\"","\""));
   }
 
   private boolean isDigit(char c) {
