f42d27a6d884713037ecf2b7031efc2124572fcb
mina0923 <cimi22051@g.nihon-u.ac.jp>
2024-12-24 00:04:37 +0900

    Parser.java,Loxjava

--- codes/com/craftinginterpreters/lox/Parser.java
+++ codes/com/craftinginterpreters/lox/Parser.java
@@ -13,6 +13,15 @@ class Parser {
   Parser(List<Token> tokens) {
     this.tokens = tokens;
   }
+
+  Expr parse() {
+    try {
+      return expression();
+    } catch (ParseError error) {
+      return null;
+    }
+  }
+  
   private Expr expression() {
     return equality();
   } 
@@ -89,6 +98,8 @@ class Parser {
       consume(RIGHT_PAREN, "Expect ')' after expression.");
       return new Expr.Grouping(expr);
     }
+    
+    throw error(peek(), "Expect expression.");
   }
   private boolean match(TokenType... types) {
     for (TokenType type : types) {
@@ -131,5 +142,25 @@ class Parser {
     Lox.error(token, message);
     return new ParseError();
   }
+  private void synchronize() {
+    advance();
 
+    while (!isAtEnd()) {
+      if (previous().type == SEMICOLON) return;
+
+      switch (peek().type) {
+        case CLASS:
+        case FUN:
+        case VAR:
+        case FOR:
+        case IF:
+        case WHILE:
+        case PRINT:
+        case RETURN:
+          return;
+      }
+
+      advance();
+    }
+  }
 }
