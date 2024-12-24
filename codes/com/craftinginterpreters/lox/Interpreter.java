756dd66ea2cf36132b8d4b3bb56117c5751b9d96
mina0923 <cimi22051@g.nihon-u.ac.jp>
2024-12-24 12:15:17 +0900

    Interpreters.java

--- codes/com/craftinginterpreters/lox/Interpreter.java
+++ codes/com/craftinginterpreters/lox/Interpreter.java
@@ -1,6 +1,25 @@
 package com.craftinginterpreters.lox;
 
-class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
+//class Interpreter implements Expr.Visitor<Object> {
+class Interpreter implements Expr.Visitor<Object>,Stmt.Visitor<Void> {
+    void interpret(List<Stmt> statements) {
+        try {
+          for (Stmt statement : statements) {
+            execute(statement);
+          }
+        } catch (RuntimeError error) {
+          Lox.runtimeError(error);
+        }
+    }
+    /*void interpret(Expr expression) { 
+    try {
+      Object value = evaluate(expression);
+      System.out.println(stringify(value));
+    } catch (RuntimeError error) {
+      Lox.runtimeError(error);
+    }
+  }*/
   @Override
   public Object visitLiteralExpr(Expr.Literal expr) {
     return expr.value;
@@ -12,26 +31,38 @@ class Interpreter implements Expr.Visitor<Object> {
 
     switch (expr.operator.type) {
       case BANG:
-      return !isTruthy(right);
+        return !isTruthy(right);
       case MINUS:
-      checkNumberOperand(expr.operator, right);
-      return -(double)right;
+        checkNumberOperand(expr.operator, right);
+        return -(double)right;
     }
-
-    // Unreachable.
     return null;
   }
+
+  private void checkNumberOperand(Token operator, Object operand) {
+    if (operand instanceof Double) return;
+    throw new RuntimeError(operator, "Operand must be a number.");
+  }
+
+  private void checkNumberOperands(Token operator, Object left, Object right) { //値でないものが来た時にチェックする
+    if (left instanceof Double && right instanceof Double) return;
+    
+    throw new RuntimeError(operator, "Operands must be numbers.");
+  }
+
   private boolean isTruthy(Object object) {
     if (object == null) return false;
     if (object instanceof Boolean) return (boolean)object;
     return true;
   }
+
   private boolean isEqual(Object a, Object b) {
     if (a == null && b == null) return true;
     if (a == null) return false;
 
     return a.equals(b);
   }
+
   private String stringify(Object object) {
     if (object == null) return "nil";
 
@@ -45,81 +76,74 @@ class Interpreter implements Expr.Visitor<Object> {
 
     return object.toString();
   }
+
   @Override
   public Object visitGroupingExpr(Expr.Grouping expr) {
     return evaluate(expr.expression);
-    private void checkNumberOperand(Token operator, Object operand) {
-      if (operand instanceof Double) return;
-      throw new RuntimeError(operator, "Operand must be a number.");
-    }
-    private void checkNumberOperands(Token operator,
-                                   Object left, Object right) {
-    if (left instanceof Double && right instanceof Double) return;
-    
-    throw new RuntimeError(operator, "Operands must be numbers.");
   }
+
   private Object evaluate(Expr expr) {
-        return expr.accept(this);
-      }
-    
+    return expr.accept(this);
+  }
+
+  private void execute(Stmt stmt) {
+    stmt.accept(this);
+  }
+  
+  @Override
+  public Void visitExpressionStmt(Stmt.Expression stmt) {
+    evaluate(stmt.expression);
+    return null;
+  }
+
+  @Override
+  public Void visitPrintStmt(Stmt.Print stmt) {
+    Object value = evaluate(stmt.expression);
+    System.out.println(stringify(value));
+    return null;
+  }
+
   @Override
   public Object visitBinaryExpr(Expr.Binary expr) {
-       Object left = evaluate(expr.left);
-       Object right = evaluate(expr.right); 
-    
-       switch (expr.operator.type) {
-        case BANG_EQUAL: return !isEqual(left, right);
-        case EQUAL_EQUAL: return isEqual(left, right);
-        case GREATER:
+    Object left = evaluate(expr.left);
+    Object right = evaluate(expr.right); 
+
+    switch (expr.operator.type) {
+      case BANG_EQUAL: return !isEqual(left, right);
+      case EQUAL_EQUAL: return isEqual(left, right);
+      case GREATER:
         checkNumberOperands(expr.operator, left, right);
         return (double)left > (double)right;
-        case GREATER_EQUAL:
+      case GREATER_EQUAL:
         checkNumberOperands(expr.operator, left, right);
         return (double)left >= (double)right;
-        case LESS:
+      case LESS:
         checkNumberOperands(expr.operator, left, right);
         return (double)left < (double)right;
-        case LESS_EQUAL:
+      case LESS_EQUAL:
         checkNumberOperands(expr.operator, left, right);
         return (double)left <= (double)right;
-        case MINUS:
+      case MINUS:
         checkNumberOperands(expr.operator, left, right);
         return (double)left - (double)right;
-        case SLASH:
-        checkNumberOperands(expr.operator, left, right);
-        return (double)left / (double)right;
-        case STAR:
-        checkNumberOperands(expr.operator, left, right);
-        return (double)left * (double)right;
-
-        case PLUS:
-       if (left instanceof Double && right instanceof Double) {
-         return (double)left + (double)right;
-                } 
+      case PLUS:
+        if (left instanceof Double && right instanceof Double) {
+          return (double)left + (double)right;
+        } 
 
         if (left instanceof String && right instanceof String) {
           return (String)left + (String)right;
         }
-        throw new RuntimeError(expr.operator,
+        throw new RuntimeError(expr.operator, 
             "Operands must be two numbers or two strings.");
-        break;
-          case SLASH:
-            return (double)left / (double)right;
-          case STAR:
-            return (double)left * (double)right;
-        }
-    
-        // Unreachable.
-        return null;
-      }   
+      case SLASH:
+        checkNumberOperands(expr.operator, left, right);
+        return (double)left / (double)right;
+      case STAR:
+        checkNumberOperands(expr.operator, left, right);
+        return (double)left * (double)right;
+    }
+    return null;
+  }
 
-      void interpret(Expr expression) { 
-        try {
-          Object value = evaluate(expression);
-          System.out.println(stringify(value));
-        } catch (RuntimeError error) {
-          Lox.runtimeError(error);
-        }
-      }
-}
 }
