3b0d45af13c1ab5262e21987d0dacd41738e0903
mina0923 <cimi22051@g.nihon-u.ac.jp>
2025-01-17 01:13:44 +0900

    Environment.java

--- codes/com/craftinginterpreters/lox/Interpreter.java
+++ codes/com/craftinginterpreters/lox/Interpreter.java
@@ -3,6 +3,9 @@ package com.craftinginterpreters.lox;
 import java.util.List;
 //class Interpreter implements Expr.Visitor<Object> {
 class Interpreter implements Expr.Visitor<Object>,Stmt.Visitor<Void> {
+
+    private Environment environment= new Environment();
+ 
     void interpret(List<Stmt> statements) {
         try {
           for (Stmt statement : statements) {
@@ -39,12 +42,17 @@ class Interpreter implements Expr.Visitor<Object>,Stmt.Visitor<Void> {
     return null;
   }
 
+  @Override
+  public Object visitVariableExpr(Expr.Variable expr) {
+    return environment.get(expr.name);
+  }
+
   private void checkNumberOperand(Token operator, Object operand) {
     if (operand instanceof Double) return;
     throw new RuntimeError(operator, "Operand must be a number.");
   }
 
-  private void checkNumberOperands(Token operator, Object left, Object right) { //値でないものが来た時にチェックする
+  private void checkNumberOperands(Token operator, Object left, Object right) { 
     if (left instanceof Double && right instanceof Double) return;
     
     throw new RuntimeError(operator, "Operands must be numbers.");
@@ -89,6 +97,26 @@ class Interpreter implements Expr.Visitor<Object>,Stmt.Visitor<Void> {
   private void execute(Stmt stmt) {
     stmt.accept(this);
   }
+
+  void executeBlock(List<Stmt> statements,
+                    Environment environment) {
+    Environment previous = this.environment;
+    try {
+      this.environment = environment;
+
+      for (Stmt statement : statements) {
+        execute(statement);
+      }
+    } finally {
+      this.environment = previous;
+    }
+  }
+  
+  @Override
+  public Void visitBlockStmt(Stmt.Block stmt) {
+    executeBlock(stmt.statements, new Environment(environment));
+    return null;
+  }
   
   @Override
   public Void visitExpressionStmt(Stmt.Expression stmt) {
@@ -102,7 +130,22 @@ class Interpreter implements Expr.Visitor<Object>,Stmt.Visitor<Void> {
     System.out.println(stringify(value));
     return null;
   }
+  @Override
+  public Void visitVarStmt(Stmt.Var stmt) {
+    Object value = null;
+    if (stmt.initializer != null) {
+      value = evaluate(stmt.initializer);
+    }
 
+    environment.define(stmt.name.lexeme, value);
+    return null;
+  }
+  @Override
+  public Object visitAssignExpr(Expr.Assign expr) {
+    Object value = evaluate(expr.value);
+    environment.assign(expr.name, value);
+    return value;
+  }
   @Override
   public Object visitBinaryExpr(Expr.Binary expr) {
     Object left = evaluate(expr.left);
